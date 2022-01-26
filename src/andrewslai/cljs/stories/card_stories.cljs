a9fae4224e8ba5a75b953ea0405aad36934add8e
Andrew Lai <alai@splashfinancial.com>
2022-01-25 19:23:27 -0600

    Setting up stories for the website

--- src/andrewslai/cljs/stories/card_stories.cljs
+++ src/andrewslai/cljs/stories/card_stories.cljs
@@ -4,17 +4,48 @@
             [reagent.core :as reagent]))
 
 (def ^:export default
-  (helper/->default {:title     "A Card Component"
+  (helper/->default {:title     "Article Cards"
                      :component article-cards/make-card
-                     :args      {:input {:title        "An example"
-                                         :article_url  "https://andrewslai.com"
-                                         :article_id   "1"
-                                         :timestamp    "2022-01-01T00:00:00"
-                                         :article_tags "research"}}}))
+                     :args      {:title        "An example with the default image"
+                                 :article_url  "https://andrewslai.com"
+                                 :article_id   "1"
+                                 :timestamp    "2022-01-01T00:00:00"
+                                 :article_tags ""}}))
 
-(defn ^:export article-cards
+;; A "Templating" example, as an alternative to the JavaScript bind syntax explained in the Storybook docs
+(defn template
   [args]
-  (let [params (-> args helper/->params)
-        input  (:input params)]
-    (reagent/as-element
-     [article-cards/make-card input])))
+  (reagent/as-element [article-cards/make-card (js->clj (helper/->params args))]))
+
+(defn copy-function
+  [f]
+  (.bind f #js {}))
+
+(defn ->story
+  [template default-values]
+  (doto (copy-function template)
+    (set! -args default-values)))
+
+(def ^:export Default
+  (->story template #js {:title        "An example with the default image"
+                         :article_tags "unrecognized"}))
+
+(def ^:export Research
+  (->story template #js {:title        "An example research article"
+                         :article_tags "research"}))
+
+(def ^:export Thoughts
+  (->story template #js {:title        "An example thoughts article"
+                         :article_tags "thoughts"}))
+
+(def ^:export Data-Analysis
+  (->story template #js {:title        "An example data-analysis article"
+                         :article_tags "data-analysis"}))
+
+(def ^:export About
+  (->story template #js {:title        "An example about me article"
+                         :article_tags "about"}))
+
+(def ^:export Archive
+  (->story template #js {:title        "An example archived article"
+                         :article_tags "archive"}))
