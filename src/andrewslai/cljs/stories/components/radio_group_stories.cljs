d1e4005de8e508da689870b84cfe884ca25632f5
Andrew Lai <alai@splashfinancial.com>
2022-02-13 11:29:31 -0600

    Add radio group

---
+++ src/andrewslai/cljs/stories/components/radio_group_stories.cljs
@@ -0,0 +1,24 @@
+(ns andrewslai.cljs.stories.components.radio-group-stories
+  (:require [andrewslai.cljs.components.radio-group :as radio-group]
+            [andrewslai.cljs.stories.helper :as helper]
+            [reagent.core :as reagent]))
+
+(def ^:export default
+  (helper/->default {:title     "Radio Group"
+                     :component radio-group/basic-radio-group
+                     :args      {}}))
+
+;; A "Templating" example, as an alternative to the JavaScript bind syntax explained in the Storybook docs
+(defn template
+  "The template is a function of arguments because Storybook understands how to
+  translate arguments into interactive controls"
+  [args]
+  (reagent/as-element [radio-group/basic-radio-group (helper/->params args)]))
+
+(def ^:export Default-Radio-Group
+  (helper/->story template {:group-name "Gender"
+                            :default    "female"
+                            :elements   [{:value "female"
+                                          :label "Female"}
+                                         {:value "male"
+                                          :label "Male"}]}))
