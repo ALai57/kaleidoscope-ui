a9b423eb4d911e6192d19422a6771e807c922415
Andrew Lai <alai@splashfinancial.com>
2022-02-12 19:07:41 -0600

    Add snackbar

---
+++ src/andrewslai/cljs/components/snackbar.cljs
@@ -0,0 +1,28 @@
+(ns andrewslai.cljs.components.snackbar
+  (:require [reagent-mui.components :refer [snackbar alert]]
+            [taoensso.timbre :refer-macros [info]]
+            [reagent.core :as reagent]))
+
+(defn clickaway?
+  [reason]
+  (= "clickaway" reason))
+
+(defn handle-close
+  [ratom event reason]
+  (when (not (clickaway? reason))
+    (reset! ratom false)))
+
+(defn basic-snackbar
+  [{:keys [message level open? auto-hide-duration]
+    :or   {level              "info"
+           open?              true
+           auto-hide-duration 6000}}]
+  (info "Creating popup")
+  (let [showing  (reagent/atom open?)]
+    (fn []
+      [snackbar {:open               @showing
+                 :auto-hide-duration auto-hide-duration
+                 :on-close           (partial handle-close showing)}
+       [alert {:severity level
+               :on-close (partial handle-close showing)}
+        message]])))
