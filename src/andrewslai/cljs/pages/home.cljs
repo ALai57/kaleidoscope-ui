99cf2ccf40c2fda96585cb2832f490f31612379b
Andrew Lai <andrew.s.lai5@gmail.com>
2023-04-01 22:28:39 -0500

    Working Three.js!

--- src/andrewslai/cljs/pages/home.cljs
+++ src/andrewslai/cljs/pages/home.cljs
@@ -1,10 +1,14 @@
 (ns andrewslai.cljs.pages.home
   (:require [andrewslai.cljs.article-cards :as cards]
-            [andrewslai.cljs.navbar :as nav]))
+            [andrewslai.cljs.components.demo :as demo]
+            [andrewslai.cljs.navbar :as nav]
+            ))
 
 (defn home [{:keys [user recent-content notification-type]}]
   [:div
    [nav/nav-bar {:user              user
                  :notification-type notification-type}]
+   [demo/demo]
+   [:div "hi"]
    [:div "Welcome to my home page"]
    ])
