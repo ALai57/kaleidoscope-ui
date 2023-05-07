63bab3d41c6bd76826b505c864462a5f453aa0b1
Andrew Lai <andrew.s.lai5@gmail.com>
2023-05-06 20:16:00 -0500

    Port pages

--- stories/kaleidoscope/ui/stories/pages/group_management_page_stories.cljs
+++ stories/kaleidoscope/ui/stories/pages/group_management_page_stories.cljs
@@ -6,48 +6,42 @@
             ["@storybook/addon-actions" :refer [action]]))
 
 (def ^:export default
-  (helper/->default {:title     "Full Pages/Group Management Page"
-                     :component groups/-group-page
-                     :args      {:user   {}
-                                 :groups [{:group-id     "group-1"
-                                           :display-name "Family"
-                                           :memberships  [{:membership-id         1
-                                                           :membership-created-at "2021-01-01"
-                                                           :alias                 "Alison"
-                                                           :email                 "Alison@email.com"}
-                                                          {:membership-id         2
-                                                           :membership-created-at "2021-01-01"
-                                                           :alias                 "Mom"
-                                                           :email                 "mom@email.com"}
-                                                          {:membership-id         3
-                                                           :membership-created-at "2021-01-01"
-                                                           :alias                 "Dad"
-                                                           :email                 "dad@email.com"}
-                                                          ]}
-                                          {:group-id     "group-2"
-                                           :display-name "NU Friends"
-                                           :memberships  [{:membership-id         1
-                                                           :membership-created-at "2021-01-01"
-                                                           :alias                 "Steve"
-                                                           :email                 "Steve@email.com"}
-                                                          {:membership-id         2
-                                                           :membership-created-at "2021-01-01"
-                                                           :alias                 "Tim"
-                                                           :email                 "Tim@email.com"}
-                                                          {:membership-id         3
-                                                           :membership-created-at "2021-01-01"
-                                                           :alias                 "Sahil"
-                                                           :email                 "Sahil@email.com"}
-                                                          ]}]}}))
-
-
-
-;; A "Templating" example, as an alternative to the JavaScript bind syntax explained in the Storybook docs
-(defn template
-  "The template is a function of arguments because Storybook understands how to
-  translate arguments into interactive controls"
-  [args]
-  (reagent/as-element [groups/-group-page (helper/->params args)]))
+  (helper/->default-story
+   {:title     "Full Pages/Group Management Page"
+    :component (fn group-page
+                 [args]
+                 [groups/-group-page (js->clj args :keywordize-keys true)])
+    :args      {:user   {}
+                :groups [{:group-id     "group-1"
+                          :display-name "Family"
+                          :memberships  [{:membership-id         1
+                                          :membership-created-at "2021-01-01"
+                                          :alias                 "Alison"
+                                          :email                 "Alison@email.com"}
+                                         {:membership-id         2
+                                          :membership-created-at "2021-01-01"
+                                          :alias                 "Mom"
+                                          :email                 "mom@email.com"}
+                                         {:membership-id         3
+                                          :membership-created-at "2021-01-01"
+                                          :alias                 "Dad"
+                                          :email                 "dad@email.com"}
+                                         ]}
+                         {:group-id     "group-2"
+                          :display-name "NU Friends"
+                          :memberships  [{:membership-id         1
+                                          :membership-created-at "2021-01-01"
+                                          :alias                 "Steve"
+                                          :email                 "Steve@email.com"}
+                                         {:membership-id         2
+                                          :membership-created-at "2021-01-01"
+                                          :alias                 "Tim"
+                                          :email                 "Tim@email.com"}
+                                         {:membership-id         3
+                                          :membership-created-at "2021-01-01"
+                                          :alias                 "Sahil"
+                                          :email                 "Sahil@email.com"}
+                                         ]}]}}))
 
 (def ^:export Example-Group-Management-Page
-  (helper/->story template {}))
+  (clj->js {}))
--- stories/kaleidoscope/ui/stories/pages/manager_page_stories.cljs
+++ stories/kaleidoscope/ui/stories/pages/manager_page_stories.cljs
@@ -5,16 +5,9 @@
             [reagent.core :as reagent]))
 
 (def ^:export default
-  (helper/->default {:title     "Full Pages/Manager Page"
-                     :component pages.manager/manager-page
-                     :args      {}}))
-
-;; A "Templating" example, as an alternative to the JavaScript bind syntax explained in the Storybook docs
-(defn template
-  "The template is a function of arguments because Storybook understands how to
-  translate arguments into interactive controls"
-  [args]
-  (reagent/as-element [pages.manager/manager-page (helper/->params args)]))
+  (helper/->default-story {:title     "Full Pages/Manager Page"
+                           :component pages.manager/manager-page
+                           :args      {:user user-profile/example-user-data}}))
 
 (def ^:export Manager-Page
-  (helper/->story template {:user user-profile/example-user-data}))
+  (js->clj {}))
