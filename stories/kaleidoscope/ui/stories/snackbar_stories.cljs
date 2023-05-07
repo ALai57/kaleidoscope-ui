decc0192a1026726f463a56d272ff82cea960f50
Andrew Lai <andrew.s.lai5@gmail.com>
2023-05-06 20:18:18 -0500

    Snackbar

--- stories/kaleidoscope/ui/stories/snackbar_stories.cljs
+++ stories/kaleidoscope/ui/stories/snackbar_stories.cljs
@@ -1,34 +1,23 @@
 (ns kaleidoscope.ui.stories.snackbar-stories
   (:require [kaleidoscope.ui.components.snackbar :as snackbar]
-            [kaleidoscope.ui.stories.helper :as helper]
-            [reagent.core :as reagent]
-            ["@storybook/addon-actions" :refer [action]]))
+            [kaleidoscope.ui.stories.helper :as helper]))
 
 (def ^:export default
-  (helper/->default {:title     "Basic Components/Snackbar"
-                     :component snackbar/basic-snackbar
-                     :args      {:open?              true
-                                 :auto-hide-duration 6000
-                                 :message            "Hello"}}))
-
-;; A "Templating" example, as an alternative to the JavaScript bind syntax explained in the Storybook docs
-(defn template
-  "The template is a function of arguments because Storybook understands how to
-  translate arguments into interactive controls"
-  [args]
-  (reagent/as-element [snackbar/basic-snackbar (helper/->params args)]))
+  (helper/->default-story
+   {:title     "Basic Components/Snackbar"
+    :component snackbar/basic-snackbar
+    :args      {:open?              true
+                :auto-hide-duration 6000
+                :message            "Hello"}}))
 
 (def ^:export Info-Snackbar
-  (helper/->story template {:open? true}))
+  (clj->js {}))
 
 (def ^:export Warn-Snackbar
-  (helper/->story template {:open? true
-                            :level "warning"}))
+  (clj->js {:args {:level "warning"}}))
 
 (def ^:export Error-Snackbar
-  (helper/->story template {:open? true
-                            :level "error"}))
+  (clj->js {:args {:level "error"}}))
 
 (def ^:export Success-Snackbar
-  (helper/->story template {:open? true
-                            :level "success"}))
+  (clj->js {:args {:level "success"}}))
