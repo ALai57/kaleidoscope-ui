089fbfad24246905f86802179ca73780389e2ea2
Andrew Lai <alai@Andrews-MacBook-Air.local>
2023-10-20 16:09:37 -0500

    Fix Manager card display

--- src/kaleidoscope/ui/components/manager.cljs
+++ src/kaleidoscope/ui/components/manager.cljs
@@ -1,13 +1,17 @@
 (ns kaleidoscope.ui.components.manager
-  (:require [reagent.core :as reagent]
-            [reagent-mui.icons.group-add :refer [group-add]]
+  (:require [goog.string :as gstr]
+            ["@mui/material/styles" :refer [useTheme]]
+            [kaleidoscope.ui.utils.core :as u]
+            [re-frame.core :refer [dispatch subscribe]]
             [reagent-mui.components :refer [box paper
                                             card
                                             card-action-area
                                             card-media
                                             card-content
+                                            container
                                             typography]]
-            [re-frame.core :refer [dispatch subscribe]]))
+            [reagent-mui.icons.group-add :refer [group-add]]
+            [reagent.core :as reagent]))
 
 (def CAPABILITIES
   [{:name        "Articles"
@@ -29,39 +33,45 @@
 
 (defn manager-card
   [{:keys [name src alt on-click description] :as capability}]
-  [box {:sx {:display          "flex"
-             "& > :not(style)" {
-                                :m      1
-                                :width  256
-                                :height 256}}}
-   [card {:elevation 10}
-    [card-action-area {:class    "bg-primary"
-                       :on-click on-click
-                       :sx       {"& > :not(style)" {:m             1
-                                                     :height        "70%"
-                                                     :width         "auto"
-                                                     :margin-left   "auto"
-                                                     :margin-right  "auto"
-                                                     :margin-top    "5px"
-                                                     :margin-bottom "5px"}}}
-     [card-media {:component "img"
-                  :src       src
-                  :alt       alt
-                  :sx        {:max-width  "100%"
-                              :max-height "142px"}}]]
-    [card-content
-     [typography {:variant "h5"}
-      name]
-     [typography {:variant "body2"
-                  :color   "text.secondary"}
-      description]]
-    ]])
+  (let [palette (:palette (u/clojurize (useTheme)))]
+    [box {:sx {:display          "flex"
+               "& > :not(style)" {
+                                  :m      1
+                                  :width  256
+                                  :height 256}}}
+     [card {:elevation 10}
+      [card-action-area {:on-click on-click
+                         :sx       {"& > :not(style)" {:m             1
+                                                       :height        "100%"
+                                                       :width         "auto"
+                                                       :margin-left   "auto"
+                                                       :margin-right  "auto"
+                                                       :margin-top    "5px"
+                                                       :margin-bottom "5px"}
+                                    "&"               {:border-radius "0px"
+                                                       :background (gstr/format "linear-gradient(4deg, %s 70%, %s 100%)"
+                                                                                (get-in palette [:primary :light])
+                                                                                (get-in palette [:accent :main]))}
+                                    }}
+       [card-media {:component "img"
+                    :src       src
+                    :alt       alt
+                    :sx        {:max-width      "100%"
+                                :max-height     "142px"}}]]
+      [card-content
+       [typography {:variant "h5"}
+        name]
+       [typography {:variant "body2"
+                    :color   "text.secondary"}
+        description]]
+      ]]))
 
 (defn manager-cards
   []
-  [:div {:style {:display         "inline-flex"
-                 :flex-wrap       "wrap"
-                 :flex-direction  "row"
-                 :justify-content "center"}}
-   (for [capability CAPABILITIES]
-     ^{:key (:name capability)} [manager-card capability])])
+  [container
+   [:div {:style {:display         "flex"
+                  :flex-wrap       "wrap"
+                  :flex-direction  "row"
+                  :justify-content "center"}}
+    (for [capability CAPABILITIES]
+      ^{:key (:name capability)} [:f> manager-card capability])]])
