f5d72078e5ad8a7dfa45ef260164a3b3d3738eab
Andrew Lai <alai@Andrews-MacBook-Air.local>
2023-10-26 10:09:58 -0500

    Add support for backgrounds

--- src/kaleidoscope/ui/components/colors/color_family.cljs
+++ src/kaleidoscope/ui/components/colors/color_family.cljs
@@ -26,35 +26,33 @@
   )
 
 (defn color-family
-  [{:keys [family-name base-color on-change]}]
+  [{:keys [family-name base-color background-colors on-change]}]
   (let [family     (leo/make-color {:name      family-name
                                     :colorKeys [base-color]
-                                    :ratios    STANDARD-RATIOS
-                                    })
-        background (leo/make-background
-                    {:name      "bg",
-                     :colorKeys ["#bababa"],
-                     :ratios    [2, 3, 4.5, 8]})
-        theme      (leo/make-theme {:colors          [family]
-                                    :backgroundColor background
-                                    :lightness       97})
-        clj-theme  (u/clojurize (.-contrastColorPairs theme) )
-        ]
-    (js/console.log "theme" theme)
+                                    :ratios    STANDARD-RATIOS})]
     [:> Item
      [:div {:style {:display "inline-flex" :padding-right "25px"}}
       [cp/basic-color-picker {:color     base-color
                               :on-change on-change}]
-      [:div {:style {:backgroundColor (:background clj-theme)
-                     :padding         "20px"}}
-       [stack {:spacing 2}
-        (for [[k color] (filter (fn [[k v]]
-                                  (not= k :background))
-                                clj-theme)]
-          [:div {:style {:backgroundColor color
-                         :width           "50px"
-                         :height          "50px"}}
-           k])]]]]))
+      (for [background-color background-colors]
+        (let [background (leo/make-background {:name      "bg",
+                                               :colorKeys [(or background-color "#bababa")],
+                                               :ratios    [2, 3, 4.5, 8]})
+              theme     (leo/make-theme {:colors          [family]
+                                         :backgroundColor background-color
+                                         :lightness       50})
+              clj-theme (u/clojurize (.-contrastColorPairs theme) )
+              _         (js/console.log "theme" theme)]
+          [:div {:style {:backgroundColor (:background clj-theme)
+                         :padding         "20px"}}
+           [stack {:spacing 2}
+            (for [[k color] (filter (fn [[k v]]
+                                      (not= k :background))
+                                    clj-theme)]
+              [:div {:style {:backgroundColor color
+                             :width           "50px"
+                             :height          "50px"}}
+               k])]]))]]))
 
 (defn color-theme
   [{:keys [theme-elements background-elements]}]
@@ -62,20 +60,17 @@
         backgrounds (reagent/atom background-elements)]
     (fn []
       [box
-       [stack {:spacing 2}
-        (for [[idx {:keys [family-name base-color] :as element}] (map-indexed (fn [idx ele]
-                                                                                [idx ele])
-                                                                              @backgrounds)]
-          [:div
-           idx
-           " BACKGROUND) "
-           family-name
-           [color-family {:family-name family-name
-                          :base-color  base-color
-                          :on-change   (fn [new-color]
-                                         (swap! colors update-in [idx] assoc :base-color (cp/upcase new-color)))
-                          }]])
-        ]
+       "BACKGROUNDS)"
+       [:> Item
+        [stack {:spacing   4
+                :direction "row"}
+         (for [[idx {:keys [family-name base-color] :as element}] (map-indexed (fn [idx ele]
+                                                                                 [idx ele])
+                                                                               @backgrounds)]
+           [:div {:style {:padding "10px"}}
+            [cp/basic-color-picker {:color     base-color
+                                    :on-change (fn [new-color]
+                                                 (swap! backgrounds update-in [idx] assoc :base-color (cp/upcase new-color)))}]])]]
        [stack {:spacing 2}
         (for [[idx {:keys [family-name base-color] :as element}] (map-indexed (fn [idx ele]
                                                                                 [idx ele])
@@ -84,9 +79,10 @@
            idx
            ") "
            family-name
-           [color-family {:family-name family-name
-                          :base-color  base-color
-                          :on-change   (fn [new-color]
-                                         (swap! colors update-in [idx] assoc :base-color (cp/upcase new-color)))
+           [color-family {:family-name       family-name
+                          :base-color        base-color
+                          :background-colors (map :base-color @backgrounds)
+                          :on-change         (fn [new-color]
+                                               (swap! colors update-in [idx] assoc :base-color (cp/upcase new-color)))
                           }]])
         ]])))
--- stories/kaleidoscope/ui/stories/colors/color_family_stories.cljs
+++ stories/kaleidoscope/ui/stories/colors/color_family_stories.cljs
@@ -5,26 +5,6 @@
             ["@storybook/addon-actions" :refer [action]]
             ))
 
-#_(def ^:export default
-    (helper/->default-story
-     {:title      "Basic Components/Color Family"
-      :component  color-family/color-family
-      :parameters {:docs
-                   {:description
-                    {:component "An RGB color family component"}}}
-
-      :args     {:color-name    "Primary"
-                 :initial-color "#FFFFFF"}
-      :argTypes {:color-name    {:description  "The name of the color family"
-                                 :defaultValue "Primary"}
-                 :initial-color {:description  "The starting hex value for the family"
-                                 :defaultValue "#FFFFFF"}}}))
-
-#_(def ^:export Primary
-    (clj->js {}))
-
-
-
 (def ^:export default
   (helper/->default-story
    {:title      "Basic Components/Color Theme"
@@ -33,15 +13,22 @@
                  {:description
                   {:component "An RGB color theme editor"}}}
 
-    :args     {:theme-elements #js[#js {:family-name "Primary"
-                                        :base-color  "#3B4173"}
-                                   #js {:family-name "Error"
-                                        :base-color  "#FFFFFF"}
-                                   #js {:family-name "Success"
-                                        :base-color  "#31BB28"}]}
-    :argTypes {:theme-elements {:description  "The Theme"
-                                :defaultValue [{:family-name "Primary"
-                                                :base-color  "#FFFFFF"}]}}}))
+    :args     {:theme-elements      #js[#js {:family-name "Primary"
+                                             :base-color  "#3B4173"}
+                                        #js {:family-name "Error"
+                                             :base-color  "#FFFFFF"}
+                                        #js {:family-name "Success"
+                                             :base-color  "#31BB28"}]
+               :background-elements #js[#js {:family-name "Gray"
+                                             :base-color  "#BBBBBB"}
+                                        #js {:family-name "Black"
+                                             :base-color  "#000000"}]}
+    :argTypes {:background-elements {:description  "The Backgrounds"
+                                     :defaultValue [{:family-name "Black"
+                                                     :base-color  "#000000"}]}
+               :theme-elements      {:description  "The Theme"
+                                     :defaultValue [{:family-name "Primary"
+                                                     :base-color  "#FFFFFF"}]}}}))
 
 (def ^:export Basic-Theme
   (clj->js {}))
