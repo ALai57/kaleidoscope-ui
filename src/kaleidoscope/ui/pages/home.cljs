062cf64ffe6eed0ade57a28e2894e6dcbf7dc09a
Andrew Lai <andrew.s.lai5@gmail.com>
2024-04-22 09:02:09 -0500

    WIP home page

--- src/kaleidoscope/ui/pages/home.cljs
+++ src/kaleidoscope/ui/pages/home.cljs
@@ -1,40 +1,75 @@
 (ns kaleidoscope.ui.pages.home
-  (:require [kaleidoscope.ui.components.article-cards :as cards]
-            [kaleidoscope.ui.components.demo :as demo]
-            [kaleidoscope.ui.components.button :as button]
-            [kaleidoscope.ui.components.navbar :as nav]
-            [kaleidoscope.ui.components.colors.color-wheel :as cw]
-            [reagent-mui.components :refer [box]]
-
-            ["@mui/material" :as mui]
-            ["@mui/material/styles" :refer [createTheme]]
-            [kaleidoscope.ui.theme :as theme]
+  (:require [kaleidoscope.ui.components.navbar :as nav]
+            [reagent-mui.components :refer [box grid paper stack typography]]
             [reagent.core :as reagent]
-            [re-frame.core :refer [dispatch]]
             ))
 
-(defn -save-theme!
-  [{:keys [hue saturation lightness angle theta] :as clj-theme}]
-  (dispatch [:save-theme! clj-theme]))
-
-(defn home [{:keys [user notification-type theme-event-handlers theme]}]
+(defn home [{:keys [user notification-type]}]
   [:div {:style {:min-height "100vh"}}
    [nav/nav-bar {:user              user
                  :notification-type notification-type}]
-   [box {:display        "flex"
-         :alignItems     "center"
-         :justifyContent "center"}
-    [:f> cw/color-wheel {:wheel-radius    200
-                         :ring-thickness  60
-                         :initial-palette theme
-                         :on-change       (:on-change theme-event-handlers)}]]
-
    [:br]
-   [:br]
+   [grid {:container      true
+          :display        "flex"
+          :justifyContent "center"
+          :alignItems     "center"
+          :align          "center"
+          :xs             12
+          :sm             12
+          :md             12
+          :lg             12
+          :xl             12}
+    [grid {:item true
+           :p    2 ;; padding
+           :xs   6
+           :sm   6
+           :md   5
+           :lg   4
+           :xl   4}
+     [box {:component      "img"
+           :display        "flex"
+           :justifyContent "center"
+           :alignItems     "center"
+           :sx             {:max-width  "100%"
+                            :max-height "100%"
+                            :content    {:xs "url(/static/images/andrew-lai-small.png)"
+                                         :sm "url(/static/images/andrew-lai-small.png)"
+                                         :md "url(/static/images/andrew-lai.jpeg)"
+                                         :lg "url(/static/images/andrew-lai.jpeg)"
+                                         :xl "url(/static/images/me-tree.png)"
+                                         }}}]]
+    [grid {:item true
+           :p    2
+           :xs   6
+           :sm   6
+           :md   5
+           :lg   4
+           :xl   4}
+     [typography
+      "Welcome to my website!"]
 
-   [box {:display        "flex"
-         :alignItems     "center"
-         :justifyContent "center"}
-    [button/button {:text     "Example button"
-                    :on-click (partial -save-theme! theme)}]]
-   #_[demo/demo]])
+     [typography
+      "I'm a software developer who loves building. I built this site because ..."]]
+    [grid {:item true
+           :p    2 ;; padding
+           :xs   12
+           :sm   12
+           :md   10
+           :lg   10
+           :xl   8}
+     [paper {:height     60
+             :lineHeight "60px"
+             :elevation  8}
+      [stack {:direction "row"}
+       [stack {:direction "column"
+               :sx        {:width "50%"}}
+        [typography {:variant "h4"}
+         "Built with"]]
+       [stack {:direction "column"
+               :sx        {:width "50%"}}
+        [typography {:variant "h4"}
+         "Deployed with"]]
+       ]
+      ]]]
+
+   ])
--- src/kaleidoscope/ui/pages/ui_manager.cljs
+++ src/kaleidoscope/ui/pages/ui_manager.cljs
@@ -1,22 +1,48 @@
 (ns kaleidoscope.ui.pages.ui-manager
-  (:require [kaleidoscope.ui.components.navbar :as nav]
+  (:require ["@mui/material/styles" :refer [styled useTheme]]
+            [kaleidoscope.ui.components.button :as button]
             [kaleidoscope.ui.components.colors.color-family :refer [color-theme]]
+            [kaleidoscope.ui.components.colors.color-wheel :as cw]
+            [kaleidoscope.ui.components.navbar :as nav]
             [kaleidoscope.ui.utils.core :as u]
-            [kaleidoscope.ui.clients.leonardo] ;; DELETEME
-            ["@mui/material/styles" :refer [styled useTheme]]
+            [reagent-mui.components :refer [box grid typography]]
+            [re-frame.core :refer [dispatch]]
             ))
 
-(defn -ui-manager-page [{:keys [user notification-type auth-token]}]
+(defn -save-theme!
+  [{:keys [hue saturation lightness angle theta] :as clj-theme}]
+  (dispatch [:save-theme! clj-theme]))
+
+(defn -ui-manager-page [{:keys [user notification-type auth-token theme-event-handlers theme]}]
   (let [palette (:palette (u/clojurize (useTheme)))]
     [:div
      [nav/nav-bar {:user              user
                    :notification-type notification-type}]
      [:div {:margin "10px"}
+      [box {:display        "flex"
+            :alignItems     "center"
+            :justifyContent "center"}
+       [:f> cw/color-wheel {:wheel-radius    200
+                            :ring-thickness  60
+                            :initial-palette theme
+                            :on-change       (:on-change theme-event-handlers)}]]
+
+      [:br]
+      [:br]
+
+      [box {:display        "flex"
+            :alignItems     "center"
+            :justifyContent "center"}
+       [button/button {:text     "Example button"
+                       :on-click (partial -save-theme! theme)}]]
+      #_[demo/demo]
 
       [color-theme {:theme-elements      {}
                     :background-elements {}}]]]))
 
 (defn ui-manager-page
-  [{:keys [user notification-type]}]
-  [:f> -ui-manager-page {:user              user
-                         :notification-type notification-type}])
+  [{:keys [user notification-type theme-event-handlers theme]}]
+  [:f> -ui-manager-page {:user                 user
+                         :theme                theme
+                         :theme-event-handlers theme-event-handlers
+                         :notification-type    notification-type}])
