d2eab31ecc85b9d3511cd4926445dcad0e6d8fa8
Andrew Lai <andrew.s.lai5@gmail.com>
2024-12-28 13:26:24 -0600

    Payment/checkout flow

--- src/kaleidoscope/pub/core.cljs
+++ src/kaleidoscope/pub/core.cljs
@@ -1,29 +1,28 @@
 (ns kaleidoscope.pub.core
   (:require
-   ;; Just to get React 18
-   ["react-dom/client" :as react-dom]
-   [goog.dom :as gdom]
+    ;; Just to get React 18
+    ["react-dom/client" :as react-dom]
+    [goog.dom :as gdom]
 
-   [kaleidoscope.pub.events.core]
-   [kaleidoscope.ui.clients.bugsnag :as bugsnag]
-   [kaleidoscope.ui.events.keycloak]
-   [kaleidoscope.ui.events.payments]
-   [kaleidoscope.ui.events.payments]
-   [kaleidoscope.ui.subs]
-   [kaleidoscope.ui.pages.sign-up :as pages.sign-up]
+    [kaleidoscope.pub.events.core]
+    [kaleidoscope.ui.clients.bugsnag :as bugsnag]
+    [kaleidoscope.ui.events.keycloak]
+    [kaleidoscope.ui.events.payments]
+    [kaleidoscope.ui.subs]
+    [kaleidoscope.ui.pages.sign-up :as pages.sign-up]
 
-   [day8.re-frame.http-fx]
-   [re-frame.core :refer [dispatch-sync subscribe]]
-   [reagent-mui.components :refer [app-bar container toolbar typography box button]]
-   [reagent.core :as r]
-   [reitit.coercion.spec :as rss]
-   [reitit.frontend :as rf]
-   [reitit.frontend.easy :as rfe]))
+    [day8.re-frame.http-fx]
+    [re-frame.core :refer [dispatch-sync subscribe]]
+    [reagent-mui.components :refer [app-bar container grid toolbar typography box button paper card card-content card-media]]
+    [reagent.core :as r]
+    [reitit.coercion.spec :as rss]
+    [reitit.frontend :as rf]
+    [reitit.frontend.easy :as rfe]))
 
 (dispatch-sync [:boot])
 
 ;; -- Debugging aids ----------------------------------------------------------
-(enable-console-print!)   ;; so that println writes to `console.log`
+(enable-console-print!)                                     ;; so that println writes to `console.log`
 
 ;; -- Entry Point -------------------------------------------------------------
 ;; Within ../../resources/public/index.html you'll see this code
@@ -41,11 +40,48 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Routing
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn example-card
+  []
+  [paper {:elevation 20}
+   [card {:sx {:display "flex"}}
+    [grid {:container true}
+     [grid {:item true
+            :xs   6}
+      [box {:sx {:display "flex" :flex-direction "column"}}
+       [card-content {:sx {:flex "1 0 auto"}}
+        [typography {:variant "h4"}
+         "Publish your content"]]]]
+     [grid {:item true
+            :xs   6}
+      [card-media {:component "img"
+                   ;;:sx        {:width 151}
+                   ;;https://picjumbo.com/download/?image=18577
+                   :image     "/static/images/ice-lollies.jpg"}]]]]])
+
 (def routes
   [["/about"
     {:name ::about
      :view (fn []
-             [:div "About"])}]
+             [grid {:container true
+                    :sx        {:height "100vh"}}
+              [grid {:item true
+                     :xs   6
+                     :sx   {:height "100%"}}
+               [paper {:sx {:height              "100%"
+                            :background-image    "url(./static/images/ice-lollies.jpg)"
+                            :background-size     "cover"
+                            :background-position "center"}}
+                [typography {:variant "h3"}
+                 "Publish your content"]]]
+              [grid {:item true
+                     :xs   6}
+               [grid {:container       true
+                      :justify-content "center"
+                      :align-items     "center"}
+                [grid {:item true
+                       :xs   8}
+                 [example-card]]]]])}]
    ["/pricing"
     {:name ::pricing
      :view (fn []
@@ -54,21 +90,24 @@
     {:name ::register
      :view (fn []
              (let [payment-details (subscribe [:payment-details])
-                   stripe          (subscribe [:stripe])]
-               [:f> pages.sign-up/-sign-up {:payment-details @payment-details
-                                            :stripe          @stripe}]))}]])
+                   domain-availability (subscribe [:domain-availability])
+                   stripe (subscribe [:stripe])]
+               (fn []
+                 [:f> pages.sign-up/-sign-up {:payment-details     @payment-details
+                                              :domain-availability @domain-availability
+                                              :stripe              @stripe}])))}]])
 
 
 (defonce match (r/atom nil))
 
 (rfe/start!
- (rf/router routes {:data {:coercion rss/coercion}})
- (fn [m]
-   ;;(js/console.log "Matched" m)
-   ;;(println "Matched" m)
-   (reset! match m))
- ;; set to false to enable HistoryAPI
- {:use-fragment false})
+  (rf/router routes {:data {:coercion rss/coercion}})
+  (fn [m]
+    ;;(js/console.log "Matched" m)
+    ;;(println "Matched" m)
+    (reset! match m))
+  ;; set to false to enable HistoryAPI
+  {:use-fragment false})
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Pages and views
@@ -79,8 +118,8 @@
    "Route does not exist"])
 
 (def pages
-  {"Pricing"  "/pricing"
-   "About"    "/about"
+  {"About"    "/about"
+   "Pricing"  "/pricing"
    "Register" "/register"})
 
 (defn root-view
@@ -95,6 +134,7 @@
       [box {:sx {:flex-grow 1
                  :display   {:xs "flex"}}}
        (for [[page-name link] pages]
+         ^{:key page-name}
          [:a {:href link}
           [button {:key      page-name
                    :on-click (fn []
--- src/kaleidoscope/ui/pages/sign_up.cljs
+++ src/kaleidoscope/ui/pages/sign_up.cljs
@@ -15,25 +15,34 @@
 (defonce stripe-init
   (dispatch [:init-global-stripe!]))
 
+(def DomainAvailableIcon
+  [:> CheckBox
+   {:color  "green"
+    :size   "60px"
+    :height "60px"}])
+
+(def DomainNotAvailableIcon
+  [:> WarningAmber
+   {:color  "red"
+    :size   "60px"
+    :height "60px"}])
+
+
 (defn stripe-payment
-  [{:keys [payment-details stripe] :as args}]
-  (let [here                    (.. js/window -location -href)
+  [{:keys [domain-availability payment-details stripe] :as args}]
+  (let [here (.. js/window -location -href)
         {:keys [intent secret]} payment-details
-        elems                   (useElements)]
+        elems (useElements)]
     (fn []
       (println "Intent" intent)
       [grid {:item true
-             :xs   12
-             :sm   10
-             :md   8
-             :lg   6
-             :xl   4}
+             :xs   12}
        [paper {:elevation 10}
         [box {:sx {:padding "20px"}}
          [typography {:variant "h4"}
           "Invoice"]
          [:p (gstr/format "$%s for %s" (/ (:amount intent) 100)
-                          (get-in payment-details [:domain-availability :domain]))]]]
+                          (:domain domain-availability))]]]
        [:br]
        [paper {:elevation 10}
         [box {:sx {:padding "20px"}}
@@ -54,31 +63,23 @@
                                    (.confirmPayment ^js stripe))
                               )}]]]])))
 
-(defn select-plan
-  [{:keys [payment-details]}]
-  (let [{:keys [secret intent domain-availability]} payment-details
 
-        domain (reagent/atom (get domain-availability :domain ""))]
+;; {:domain safeafsesafesa.com,
+;;  :available true,
+;;  :prices {:registration-price {:price 14, :currency USD},
+;;           :transfer-price {:price 14, :currency USD},
+;;           :renewal-price {:price 14, :currency USD},
+;;           :change-ownership-price {:price 0, :currency USD},
+;;           :restoration-price {:price 57, :currency USD},
+;;           :name com}}
+(defn domain-name-selector
+  [{:keys [domain-availability next-step previous-step]}]
+  (let [domain (reagent/atom (get domain-availability :domain ""))]
     (fn []
-      ;;(println "Payment details" payment-details)
       [paper {:elevation 10}
        [grid {:container  true
               :alignItems "center"
               :padding    "10px"}
-        [grid {:item           true
-               :xs             12
-               :padding-bottom "10px"}
-         #_[typography {:variant "h4"}
-            "Select a domain name"]
-         ;;[divider]
-         #_[typography {:variant "p"}
-            "For top level domain name market prices (`.com`, `.net` etc) see "
-            [:a {:href "https://d32ze2gidvkk54.cloudfront.net/Amazon_Route_53_Domain_Registration_Pricing_20140731.pdf"}
-             "this AWS pricing document."]]]
-        [grid {:item true
-               :xs 12}
-         [typography {:variant "h5"}
-          "Domain availability checker"]]
         [grid {:item true
                :xs   6}
          [text-field {:size     "small"
@@ -89,97 +90,111 @@
                       :onChange (fn [ev]
                                   (reset! domain (events/event-value ev)))}]]
         [grid {:item           true
-               :xs             4
+               :xs             6
                :padding-bottom "10px"}
-         [button {:on-click (fn generate-payment! [e]
+         [button {:on-click (fn get-domain-price [e]
                               (.preventDefault e)
-                              (dispatch [:generate-payment-for-domain! @domain]))
-                  :text "Check"}]]
-        [grid {:item           true
-               :xs             2
-               :justifyContent "left"}
-         (if (:available domain-availability)
-           [tooltip {:title (gstr/format "Domain `%s` is available" @domain)}
-            [:> CheckBox {:color  "green"
-                          :size   "60px"
-                          :height "60px"}]]
-           [tooltip {:title (gstr/format "Domain `%s` is not available" @domain)}
-            [:> WarningAmber {:color  "red"
-                              :size   "60px"
-                              :height "60px"}]])
-         ]]])))
+                              (dispatch [:get-domain-price @domain]))
+                  :text     (if (or (empty? domain-availability)
+                                    (= @domain (:domain domain-availability)))
+                              "Check availability"
+                              "Re-check availability")}]]]
+       (let [available (:available domain-availability)
+             price (get-in domain-availability [:prices :registration-price :price])]
+         [grid {:container  true
+                :alignItems "center"
+                :padding    "10px"}
+          [grid {:item           true
+                 :xs             2
+                 :justifyContent "left"}
 
-(def steps
-  [{:label   "Select a domain name"
-    :caption [typography {:variant "caption"}
-              "For recent top level domain name market prices (`.com`, `.net` etc) refer to "
-              [:a {:href "https://d32ze2gidvkk54.cloudfront.net/Amazon_Route_53_Domain_Registration_Pricing_20140731.pdf"}
-               "this AWS document."]]
-    :content select-plan}
-   {:label       "View invoice"
-    :description "Verify the purchase price for the domain"}
-   {:label       "Payment"
-    :description "Pay to register the domain and set it up"}])
+           (if available
+             [tooltip {:title (gstr/format "Domain `%s` is available" @domain)}
+              DomainAvailableIcon]
+             [tooltip {:title (gstr/format "Domain `%s` is not available" @domain)}
+              DomainNotAvailableIcon])]
+          [grid {:item           true
+                 :xs             2
+                 :justifyContent "left"}
+           [button {:disabled (not available)
+                    :on-click next-step
+                    :text     (gstr/format "Purchase %s for $%s" (get domain-availability :domain) (or price "--"))}]]])])))
 
-(defn -sign-up
-  [{:keys [payment-details stripe] :as args}]
-  (let [{:keys [secret intent domain-availability]} payment-details]
-    [grid {:container true
-           :align-items "center"
-           :justify-content "center"}
-     [stepper {:active-step 0
-               :orientation "vertical"}
-      (for [stage steps]
-        [step {:key (:label stage)}
-         [:<>
-          [step-label {:optional (when-let [caption (:caption stage)]
-                                   (reagent/as-element caption))}
-           (:label stage)]
-          [step-content {}
-           [typography (:description stage)]
+(defn payment-widget
+  [{:keys [domain-availability payment-details stripe next-step previous-step] :as args}]
+  (let [{:keys [secret intent]} payment-details]
+    [grid {:container      true
+           :alignItems     "center"
+           :justifyContent "center"}
+     (cond
+       (and secret intent) [:> Elements {:stripe  stripe
+                                         :options {:clientSecret (:client-secret secret)
+                                                   :loader       "always"}}
+                            [:f> stripe-payment args]]
 
-           ^{:key domain-availability}
-           (when-let [content (:content stage)]
-             [:f> content {:payment-details payment-details}])]]])]]
-
-    #_[grid {:container      true
-             :alignItems     "center"
-             :justifyContent "center"}
-       #_[grid {:item true
-                :xs   12
-                :sx   {:paddingBottom "25px"}}
-          [typography {:textAlign "center"
-                       :variant   "h4"}
-           "Sign up"]]
+       (:available domain-availability)
        [grid {:item true
               :xs   12
-              :sx   {:margin-bottom "15px"}}
-        [grid {:container      true
-               :alignItems     "center"
-               :justifyContent "center"}
-         [grid {:item true
-                :xs   12
-                :sm   10
-                :md   8
-                :lg   6
-                :xl   4}
-          ^{:key domain-availability}
-          [:f> select-plan {:payment-details payment-details}]]]]
+              :sm   10
+              :md   8
+              :lg   6
+              :xl   4}
+        [:h2 "Loading payments"]
+        [circular-progress]]
+       )]))
 
-       (cond
-         (and secret intent)              [:> Elements {:stripe  stripe
-                                                        :options {:clientSecret (:client-secret secret)
-                                                                  :loader       "always"}}
-                                           [:f> stripe-payment args]]
-         (:available domain-availability) [grid {:item true
-                                                 :xs   12
-                                                 :sm   10
-                                                 :md   8
-                                                 :lg   6
-                                                 :xl   4}
-                                           [:h2 "Loading payments"]
-                                           [circular-progress]]
-         )])
+(def active-step
+  (reagent/atom 0))
+
+(def steps
+  "Steps defines each step in the stepper component -
+   Each step shows up a step in a linear progress bar"
+  [{:label         "Select a domain name"
+    :caption       [typography {:variant "caption"}
+                    "For recent top level domain name market prices (`.com`, `.net` etc) refer to "
+                    [:a {:href "https://d32ze2gidvkk54.cloudfront.net/Amazon_Route_53_Domain_Registration_Pricing_20140731.pdf"}
+                     "this AWS document."]]
+    :content       domain-name-selector
+    :next-step     (fn []
+                     (println "Next step" @active-step)
+                     (swap! active-step inc)
+                     (dispatch [:generate-payment-for-domain!]))
+    :previous-step (fn []
+                     (println "No previous steps"))
+    }
+   {:label         "Payment"
+    :caption       [typography {:variant "caption"}
+                    "All payment processing is handled by " [:a {:href "https://stripe.com"}
+                     "Stripe"]]
+    :content       payment-widget
+    :next-step     (fn []
+                     (println "No next steps"))
+    :previous-step (fn []
+                     (println "Prev step" @active-step)
+                     (swap! active-step dec))
+    }])
+
+(defn -sign-up
+  [{:keys [payment-details stripe domain-availability] :as args}]
+  [grid {:container       true
+         :align-items     "center"
+         :justify-content "center"}
+   [stepper {:active-step @active-step
+             :orientation "vertical"}
+    (for [{:keys [content label caption description next-step previous-step] :as stage} steps]
+      [step {:key label}
+       [:<>
+        [step-label {:optional (when caption (reagent/as-element caption))}
+         label]
+        [step-content {}
+         [typography description]
+
+         (when content
+           ^{:key args}
+           [:f> content (assoc args
+                          :next-step next-step
+                          :previous-step previous-step)])
+         ]]])]]
 
   )
 
@@ -190,7 +205,7 @@
 (defn sign-up
   [{:keys [user notification-type]}]
   (let [payment-details (subscribe [:payment-details])
-        stripe          (subscribe [:stripe])]
+        stripe (subscribe [:stripe])]
     (fn []
       [:div {:style {:height   "100%"
                      :width    "100%"
