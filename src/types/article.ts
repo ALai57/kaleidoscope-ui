d9eef848264d3889d7436e2ce70bda7c90e15522
Andrew Lai <andrew.s.lai5@gmail.com>
2026-04-08 07:12:30 -0500

    feat(types): add Article, ArticleBranch, ArticleVersion types

---
+++ src/types/article.ts
@@ -0,0 +1,24 @@
+export interface Article {
+  article_id: string;
+  article_url: string;
+  article_title: string;
+  article_tags: string;
+  public_visibility: boolean;
+  created_at: string;
+}
+
+export interface ArticleBranch {
+  branch_id: string;
+  branch_name: string;
+  article_id: string;
+  article_url: string;
+}
+
+export interface ArticleVersion {
+  version_id: string;
+  branch_id: string;
+  content: string;
+  title: string;
+  article_tags?: string;
+  created_at: string;
+}
